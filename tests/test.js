bd83b6d3e72910efcd32d987d9e9dde23b623047
ChuckNorris <nicolai.fricker@gmx.de>
2018-09-26 22:01:24 +0200

    removed tests

---
+++ tests/test.js
@@ -0,0 +1,13 @@
+var server   = require('../server'),
+    chai     = require('chai'),
+    chaiHTTP = require('chai-http'),
+    should   = chai.should();
+
+chai.use(chaiHTTP);
+
+reqServer = process.env.HTTP_TEST_SERVER || server
+
+describe('Basic routes tests', function() {
+
+
+})
